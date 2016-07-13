c9f1fee3e9a6c9ce55950d86d26040230d93c5d3
beszczur <beszczur@gmail.com>
2016-07-13 16:38:23 +0200

    Second commit

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ MyFristProject
 A Symfony project created on July 13, 2016, 6:30 am.
 =======
 # FirstStepsInSymfony
+
+
+TO DO List:
+1. Attach .css file 
+2. Attach Entity/User (Validation)
 >>>>>>> 581ab3af093967e07d908622de695426d0ae9b72
--- src/UserBundle/Controller/PostController.php
+++ src/UserBundle/Controller/PostController.php
@@ -2,11 +2,18 @@
 
 namespace UserBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UserBundle\Entity\User2;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+//use UserBundle\Entity\User;
 
 class PostController extends Controller
 {
@@ -32,10 +39,44 @@ class PostController extends Controller
         	return new Response ("Gratki!");
 	else
 		return new Response ("Nie gratki");
-/*$this->render('UserBundle:Post:user_index.html.twig', array(
-            // ...
-        ));*/
     }
+
+    /**
+     * @Route("/Register")
+     * @Method({"GET", "POST"})
+     */
+
+    public function RegisterForm(Request $request)
+    {
+	$user = new User2();
+	$user->setLogin('beata');
+	$user->setFirstName('Beatowa');
+
+	$form = $this->createFormBuilder($user)
+		->add('login', TextType::class)
+		->add('firstName', TextType::class)
+		->add('Submit', SubmitType::class)
+		->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+   
+            return new Response("You are MASTER");
+	    //$this->redirect($this->generateUrl('task_success'));
+/*Przekierowanie użytkownika po udanym zgłoszeniu formularza uniemożliwia użytkownikowi, by odświeżył i ponownie przesłał dane.*/
+        }
+
+	return $this->render('UserBundle:Post:user_register.html.twig', array(
+	'form' => $form->createView(),
+            // ...
+        ));
+    }
+
+
+
+
 
 
 
